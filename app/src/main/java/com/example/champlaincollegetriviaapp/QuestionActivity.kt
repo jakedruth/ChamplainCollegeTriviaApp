46114285c998a0170e6d13bea5bd125a94479daf
Jakedruth@gmail.com <Jakedruth@gmail.com>
2020-10-15 11:43:50 -0400

    implemented xml reading. Now to refactor code

--- app/src/main/java/com/example/champlaincollegetriviaapp/QuestionActivity.kt
+++ app/src/main/java/com/example/champlaincollegetriviaapp/QuestionActivity.kt
@@ -1,29 +1,107 @@
 package com.example.champlaincollegetriviaapp
 
+import android.content.Context
+import android.content.res.AssetManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.geoquiz.Question
+import com.bignerdranch.android.geoquiz.QuizViewModel
+import java.io.File
+import org.xmlpull.v1.XmlPullParserException
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+import java.io.IOException
+import java.io.InputStream
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 class QuestionActivity : AppCompatActivity() {
+
+    lateinit var questionCounterTextView: TextView
+    lateinit var questionTextView: TextView
+    lateinit var choicesButtons: Array<Button>
+
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
 
-        val question: Question = Question("The correct answer is A", arrayOf<String> ("A", "B", "C", "D"), 0, "multipleChoice")
+        questionCounterTextView = findViewById<TextView>(R.id.question_counter)
+        questionTextView = findViewById<TextView>(R.id.question_title)
+        choicesButtons = arrayOf<Button>(findViewById<Button>(R.id.choice_0), findViewById<Button>(R.id.choice_1), findViewById<Button>(R.id.choice_2), findViewById<Button>(R.id.choice_3))
+        for (i in choicesButtons.indices) {
+            choicesButtons[i].setOnClickListener { handleButtonClick(i) }
+        }
 
-        var counterText = getString(R.string.question_counter)
+        parseXMLQuestions("questions_buildings.xml")
+
+        displayQuestion(quizViewModel.buildingQuiz.getCurrentQuestion())
+
+        //val question: Question = Question("The correct answer is A", arrayOf<String> ("A", "B", "C", "D"), 0, "multipleChoice")
+
+        //displayQuestion(question)
+    }
+
+    private fun parseXMLQuestions(filePath: String) {
+        // XML Parser citation: https://www.javatpoint.com/kotlin-android-xmlpullparser-tutorial
+        var question: Question? = null
+        var text: String? = null
+
+        try {
+            val factory = XmlPullParserFactory.newInstance()
+            val parser = factory.newPullParser()
+            parser.setInput(assets.open(filePath), null)
+            var eventType = parser.eventType
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                val tagName = parser.name
+                when (eventType) {
+                    XmlPullParser.START_TAG -> if (tagName.equals("question", ignoreCase = true)) {
+                        // Create New Question
+                        question = Question()
+                        question.correctAnswer = parser.getAttributeValue(null, "correctID").toInt()
+                    }
+                    XmlPullParser.TEXT -> text = parser.text
+                    XmlPullParser.END_TAG -> if (tagName.equals("question", ignoreCase = true)) {
+                        question?.let { quizViewModel.buildingQuiz.questions.add(question) }
+                    } else if (tagName.equals("text", ignoreCase = true)) {
+                        question!!.text = text!!
+                    } else if (tagName.equals("choice", ignoreCase = true)) {
+                        question!!.choices.add(text!!)
+                    }
+                    else -> {
+                    }
+                }
+
+                eventType = parser.next()
+            }
+        } catch (e:XmlPullParserException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun handleButtonClick(index: Int) {
+
+    }
+
+    private fun displayQuestion(question: Question) {
+        var counterText: String = getString(R.string.question_counter)
         counterText = counterText.replace("@a", 0.toString())
         counterText = counterText.replace("@b", 10.toString())
-        findViewById<TextView>(R.id.question_counter).text = counterText
+        questionCounterTextView.text = counterText
 
-        findViewById<TextView>(R.id.question_title).text = question.text;
+        questionTextView.text = question.text;
 
-        findViewById<Button>(R.id.choice_0).text = question.choices[0];
-        findViewById<Button>(R.id.choice_1).text = question.choices[1];
-        findViewById<Button>(R.id.choice_2).text = question.choices[2];
-        findViewById<Button>(R.id.choice_3).text = question.choices[3];
+        for (i in choicesButtons.indices) {
+            choicesButtons[i].text = question.choices[i]
+        }
     }
 }
--- app/src/main/java/com/example/champlaincollegetriviaapp/QuizViewModel.kt
+++ app/src/main/java/com/example/champlaincollegetriviaapp/QuizViewModel.kt
@@ -5,34 +5,64 @@ import androidx.lifecycle.ViewModel
 
 private const val TAG = "QuizViewModel"
 class QuizViewModel : ViewModel() {
-    val category: String = ""
-    val questionIndex: Int = 0
+    val buildingQuiz: Quiz = Quiz("Buildings")
+}
+
+class Quiz {
+    var category: String = ""
+    var questions: ArrayList<Question> = ArrayList()
+    var currentQuestionIndex: Int = 0
+
+    constructor(category: String) {
+        this.category = category
+    }
+
+    public fun start() {
+        currentQuestionIndex = 0
+    }
+
+    public fun getCurrentQuestion(): Question {
+        return questions[currentQuestionIndex]
+    }
+
+    public fun nextQuestion() {
+        currentQuestionIndex++
+        if (currentQuestionIndex >= questions.size)
+            currentQuestionIndex = questions.size - 1
+    }
+}
+
+class Question {
+    var text: String = ""
+    var correctAnswer: Int = 0
+    var choices: ArrayList<String> = ArrayList()
+
 
 
 }
 
-data class Question(val text: String, val choices: Array<String>, val answer: Int, val questionType: String) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Question
-
-        if (text != other.text) return false
-        if (!choices.contentEquals(other.choices)) return false
-        if (answer != other.answer) return false
-        if (questionType != other.questionType) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = text.hashCode()
-        result = 31 * result + choices.contentHashCode()
-        result = 31 * result + answer
-        result = 31 * result + questionType.hashCode()
-        return result
-    }
-
-}
+//data class Question(val text: String, val choices: Array<String>, val answer: Int, val questionType: String) {
+//
+//    override fun equals(other: Any?): Boolean {
+//        if (this === other) return true
+//        if (javaClass != other?.javaClass) return false
+//
+//        other as Question
+//
+//        if (text != other.text) return false
+//        if (!choices.contentEquals(other.choices)) return false
+//        if (answer != other.answer) return false
+//        if (questionType != other.questionType) return false
+//
+//        return true
+//    }
+//
+//    override fun hashCode(): Int {
+//        var result = text.hashCode()
+//        result = 31 * result + choices.contentHashCode()
+//        result = 31 * result + answer
+//        result = 31 * result + questionType.hashCode()
+//        return result
+//    }
+//
+//}
